7f5b3d9a24d7345fa0af21554c5e9f8a57fa63c7
Jeemlei <eemeli.ingervo@gmail.com>
2022-12-30 16:56:06 +0200

    Combining actions jobs

--- backend/src/app.py
+++ backend/src/app.py
@@ -6,28 +6,43 @@ app = Flask(__name__, static_url_path='', static_folder='../static')
 if config.DEV:
     CORS(app)
 
-#API routes
+
+# Frontend and static files
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path>')
+def index(path):
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route('/manifest.json')
+def manifest():
+    return send_from_directory(app.static_folder, 'manifest.json')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.static_folder, 'favicon.ico')
+
+
+@app.route('/logo192.png')
+def logo192():
+    return send_from_directory(app.static_folder, 'logo192.png')
+
+
+@app.route('/logo512.png')
+def logo512():
+    return send_from_directory(app.static_folder, 'logo512.png')
+
+
+# API routes
+
+
 @app.route('/api/ping')
 def ping():
     return 'pong'
 
-#Frontend and static files
-@app.route('/manifest.json')
-def manifest():
-    return send_from_directory(app.static_folder, 'manifest.json')  
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(app.static_folder, 'favicon.ico') 
-@app.route('/logo192.png')
-def logo192():
-    return send_from_directory(app.static_folder, 'logo192.png')  
-@app.route('/logo512.png')
-def logo512():
-    return send_from_directory(app.static_folder, 'logo512.png')  
-@app.route('/', defaults={'path': ''})
-@app.route('/<path>')
-def index(path):
-    return send_from_directory(app.static_folder, 'index.html')  
 
 if __name__ == '__main__':
-    app.run()  
+    app.run()
