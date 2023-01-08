7698b98006b8d1e0dc69dfbd95872cebf5a0c8bd
Jeemlei <eemeli.ingervo@gmail.com>
2023-01-08 11:44:35 +0200

    Expanded station endpoint to return more details
    -Total number of journeys starting from the station.
    -Total number of journeys ending at the station.
    -The average distance of a journey starting from the station.
    -The average distance of a journey ending at the station.

--- backend/src/services/stations_service.py
+++ backend/src/services/stations_service.py
@@ -3,13 +3,35 @@ from services.tools import format_station_result
 
 
 def get_stations():
-    result = db.session.execute('SELECT * FROM stations ORDER BY id').fetchall()
+    result = db.session.execute(
+        'SELECT * FROM stations ORDER BY id').fetchall()
     return list(map(lambda station: format_station_result(station), result))
 
 
 def get_station_by_id(id):
     result = db.session.execute(
-        f'SELECT * FROM stations WHERE id={id}').fetchone()
+        f'SELECT *, \
+                 (SELECT COUNT(*) \
+                  FROM journeys \
+                  WHERE departure_station={id}) as starting_journeys, \
+                 (SELECT COUNT(*) \
+                  FROM journeys \
+                  WHERE return_station={id}) as ending_journeys, \
+                 (SELECT AVG(distance) \
+                  FROM journeys \
+                  WHERE departure_station={id}) as avg_starting_distance, \
+                 (SELECT AVG(distance) \
+                  FROM journeys \
+                  WHERE return_station={id}) as avg_ending_distance \
+          FROM stations \
+          WHERE id={id}').fetchone()
+
     if not result:
         return 'Station not found', 404
-    return format_station_result(result)
+
+    station = format_station_result(result)
+    station['starting_journeys'] = result[-4]
+    station['ending_journeys'] = result[-3]
+    station['avg_starting_distance'] = round(float(result[-2]))
+    station['avg_ending_distance'] = round(float(result[-1]))
+    return station
