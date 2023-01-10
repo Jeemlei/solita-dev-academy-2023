ecc186f4f6e4cec6a8250bf7e0def204261ad1ef
Jeemlei <eemeli.ingervo@gmail.com>
2023-01-10 21:42:51 +0200

    Fixed rounding error in JourneyList

--- frontend/src/components/JourneyList/ListItem.tsx
+++ frontend/src/components/JourneyList/ListItem.tsx
@@ -10,8 +10,8 @@ const ListItem = ({ journey }: Props) => {
 		<tr>
 			<td>{journey.departure_station}</td>
 			<td>{journey.return_station}</td>
-			<td>{Math.round(journey.distance / 1000).toFixed(2)}</td>
-			<td>{Math.round(journey.duration / 60).toFixed(0)}</td>
+			<td>{(journey.distance / 1000).toFixed(2)}</td>
+			<td>{(journey.duration / 60).toFixed(0)}</td>
 		</tr>
 	)
 }
